09d24ea121b91f3d4549297844bbd4ab6e279874
Martin Maxa <maxama999@gmail.com>
2026-07-11 21:24:41 +0200

    feat: seed pentathlon discipline names

---
+++ migrations/0002_seed_disciplines.sql
@@ -0,0 +1,6 @@
+-- Reálné názvy disciplín hospodského pětiboje.
+UPDATE disciplines SET name = 'Hod pivním táckem'            WHERE id = 1;
+UPDATE disciplines SET name = 'Pivní štafeta'                WHERE id = 2;
+UPDATE disciplines SET name = 'Držení tupláku v předpažení'  WHERE id = 3;
+UPDATE disciplines SET name = 'Pití piva brčkem'             WHERE id = 4;
+UPDATE disciplines SET name = 'Šipky'                        WHERE id = 5;
